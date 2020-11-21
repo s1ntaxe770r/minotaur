d96db3bd747b7c6c4488be6105f1667154987b35
s1ntaxe770r <cyberdev01@protonmail.com>
2020-11-21 07:45:13 +0100

    :recycle:

--- main.go
+++ main.go
@@ -1,16 +1,22 @@
 package main
 
 import (
+	"log"
+	"net/http"
+
 	"github.com/gorilla/mux"
+	"github.com/s1ntaxe770r/PPI/handlers"
 )
 
 func main() {
 
 	r := mux.NewRouter()
 
-	r.HandleFunc("/projects", handlers.getprojects).Methods("GET")
-	r.HandleFunc("/projects", handlers.createproject).Methods("POST")
-	r.HandleFunc("/projects{id}", handlers.deleteproject).Methods("DELETE")
-	r.HandleFunc("/projects{id}", handlers.updateproject).Methods("PUT")
+	r.HandleFunc("/projects", handlers.GetProjects).Methods("GET")
+	r.HandleFunc("/projects", handlers.CreateProject).Methods("POST")
+	r.HandleFunc("/projects/{id}", handlers.DeleteProject).Methods("DELETE")
+	r.HandleFunc("/projects/{id}", handlers.UpdateProject).Methods("PUT")
 
+	log.Println("listening on :4000")
+	log.Fatal(http.ListenAndServe(":4000", r))
 }

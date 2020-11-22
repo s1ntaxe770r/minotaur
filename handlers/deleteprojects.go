79cf4e4a83da1359212687a043884fd17f1d5a20
s1ntaxe770r <cyberdev01@protonmail.com>
2020-11-22 19:55:21 +0100

    add delete handler

--- handlers/deleteprojects.go
+++ handlers/deleteprojects.go
@@ -2,12 +2,29 @@ package handlers
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/s1ntaxe770r/PPI/db"
 )
 
-// DeleteProjects handles removal of projects
+// DeleteProject handles removal of projects
 func DeleteProject(resp http.ResponseWriter, req *http.Request) {
-
-	fmt.Fprint(resp, "DELETE")
-
+	dbcon := db.Connect()
+	vars := mux.Vars(req)
+	id := vars["id"]
+	fmt.Printf("id = %s", id)
+	var project db.Project
+	jrsp := dbcon.Delete(&project, "id = ?", id).Error
+	if jrsp != nil {
+		ex := jrsp.Error()
+		log.Println(ex)
+		resp.Header().Set("Content-Type", "application/json")
+		resp.WriteHeader(http.StatusInternalServerError)
+		http.Error(resp, "could not delete project with specified id", http.StatusInternalServerError)
+		log.Println(jrsp)
+		return
+	}
+	resp.WriteHeader(http.StatusNoContent)
 }

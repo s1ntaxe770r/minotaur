f4aaa74441f958de49c35afcb008649772ef58ad
s1ntaxe770r <cyberdev01@protonmail.com>
2020-11-28 03:02:07 +0100

    updates

--- handlers/createprojects.go
+++ handlers/createprojects.go
@@ -1,13 +1,34 @@
 package handlers
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
+
+	"github.com/s1ntaxe770r/PPI/db"
+	"gopkg.in/go-playground/validator.v9"
 )
 
-// CreateProjects handles new projects additons
+// CreateProject handles new projects additons
 func CreateProject(resp http.ResponseWriter, req *http.Request) {
-
-	fmt.Fprint(resp, "POST")
+	dbcon := db.Connect()
+	var project db.Project
+	v := validator.New()
+	decdr := json.NewDecoder(req.Body).Decode(&project)
+	if decdr != nil {
+		log.Println(decdr.Error())
+		resp.WriteHeader(http.StatusBadRequest)
+		fmt.Fprintf(resp, "could not read request body")
+	}
+	err := v.Struct(project)
+	if err != nil {
+		http.Error(resp, err.Error(), http.StatusBadRequest)
+	}
+	dberr := dbcon.Create(&project).Error
+	handle(dberr)
+	resp.Header().Set("Content-Type", "application/json")
+	resp.WriteHeader(http.StatusOK)
+	json.NewEncoder(resp).Encode(project)
 
 }

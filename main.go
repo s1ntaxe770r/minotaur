40e2fdd9fdcc5a06ad7a763bb0d38fb6d824ec80
s1ntaxe770r <cyberdev01@protonmail.com>
2020-12-06 20:46:59 +0100

    use requel

--- main.go
+++ main.go
@@ -4,15 +4,18 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/fatih/color"
 	"github.com/gorilla/mux"
 	"github.com/s1ntaxe770r/PPI/handlers"
 	"github.com/s1ntaxe770r/PPI/utils"
+	"github.com/s1ntaxe770r/requel"
 )
 
 func main() {
 	envconf := utils.NewConfig()
 	envconf.LoadEnv()
 	r := mux.NewRouter()
+	r.Use(requel.LogReq)
 
 	r.HandleFunc("/projects", handlers.GetProjects).Methods("GET")
 	r.HandleFunc("/projects", handlers.CreateProject).Methods("POST")
@@ -20,7 +23,7 @@ func main() {
 	r.HandleFunc("/projects/{id}", handlers.DeleteProject).Methods("DELETE")
 	r.HandleFunc("/projects/{id}", handlers.UpdateProject).Methods("PUT")
 
-	log.Printf("server started on %s", envconf.ServerPort)
+	color.Green("server started on %s", envconf.ServerPort)
 	log.Fatal(http.ListenAndServe(envconf.ServerPort, r))
 
 }

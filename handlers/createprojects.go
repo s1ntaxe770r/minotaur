9f90bd86e4553ed6dbdfe086951f0f4b4a0d1345
s1ntaxe770r <cyberdev01@protonmail.com>
2020-12-08 19:50:00 +0100

    fixes POST request bug

--- handlers/createprojects.go
+++ handlers/createprojects.go
@@ -2,7 +2,7 @@ package handlers
 
 import (
 	"encoding/json"
-	"io/ioutil"
+	"fmt"
 	"log"
 	"net/http"
 
@@ -14,23 +14,19 @@ import (
 func CreateProject(resp http.ResponseWriter, req *http.Request) {
 	dbcon := db.Connect()
 	var project db.Project
+	json.NewDecoder(req.Body).Decode(&project)
+	fmt.Println(project)
 	v := validator.New()
-	reqbody, err := ioutil.ReadAll(req.Body)
-	json.Unmarshal(reqbody, &project)
-	if err != nil {
-		log.Println(err)
-		log.Println(project)
-		http.Error(resp, "could not read request body", http.StatusBadRequest)
-		return
-	}
 	vlderr := v.Struct(project)
 	if vlderr != nil {
 		log.Println(vlderr)
 		http.Error(resp, vlderr.Error(), http.StatusBadRequest)
 		return
 	}
+
 	dberr := dbcon.Create(&project).Error
 	handle(dberr)
+	dbcon.Commit()
 	resp.Header().Set("Content-Type", "application/json")
 	resp.WriteHeader(http.StatusOK)
 	json.NewEncoder(resp).Encode(project)

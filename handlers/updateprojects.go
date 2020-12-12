c860cb771c47a680ee1aabeca0e77ab6a23f114d
s1ntaxe770r <cyberdev01@protonmail.com>
2020-12-12 07:47:21 +0100

    implement update

--- handlers/updateprojects.go
+++ handlers/updateprojects.go
@@ -1,14 +1,25 @@
 package handlers
 
 import (
-	"fmt"
+	"encoding/json"
 	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/s1ntaxe770r/PPI/db"
 )
 
 // UpdateProjects handles project updates
 
 func UpdateProject(resp http.ResponseWriter, req *http.Request) {
-
-	fmt.Fprint(resp, "PUT")
+	dbcon := db.Connect()
+	vars := mux.Vars(req)
+	id := vars["id"]
+	var project db.Project
+	json.NewDecoder(req.Body).Decode(&project)
+	qry := dbcon.First(&project, "id = ?", id)
+	err := qry.Save(&project).Error
+	handle(err)
+	json.NewEncoder(resp).Encode(project)
+	return
 
 }

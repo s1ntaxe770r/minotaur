79762a5ba53d37c519debb8d023ed2d777015b01
s1ntaxe770r <cyberdev01@protonmail.com>
2020-12-12 07:46:53 +0100

    clean up handler

--- handlers/deleteprojects.go
+++ handlers/deleteprojects.go
@@ -1,8 +1,6 @@
 package handlers
 
 import (
-	"fmt"
-	"log"
 	"net/http"
 
 	"github.com/gorilla/mux"
@@ -14,16 +12,12 @@ func DeleteProject(resp http.ResponseWriter, req *http.Request) {
 	dbcon := db.Connect()
 	vars := mux.Vars(req)
 	id := vars["id"]
-	fmt.Printf("id = %s", id)
 	var project db.Project
 	jrsp := dbcon.Delete(&project, "id = ?", id).Error
 	if jrsp != nil {
-		ex := jrsp.Error()
-		log.Println(ex)
 		resp.Header().Set("Content-Type", "application/json")
 		resp.WriteHeader(http.StatusInternalServerError)
 		http.Error(resp, "could not delete project with specified id", http.StatusInternalServerError)
-		log.Println(jrsp)
 		return
 	}
 	resp.WriteHeader(http.StatusNoContent)
